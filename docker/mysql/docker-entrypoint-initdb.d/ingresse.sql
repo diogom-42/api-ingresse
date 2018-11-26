32fda04453ba135918de4a071b26a2b023a92682
Diogo Moura <diogomoura167@gmail.com>
2018-11-26 12:00:20 -0200

    Criacao do BD Auto. & Config. do SSL

--- docker/mysql/docker-entrypoint-initdb.d/ingresse.sql
+++ docker/mysql/docker-entrypoint-initdb.d/ingresse.sql
@@ -25,4 +25,10 @@
 #CREATE DATABASE IF NOT EXISTS `dev_db_3` COLLATE 'utf8_general_ci' ;
 #GRANT ALL ON `dev_db_3`.* TO 'default'@'%' ;
 
-FLUSH PRIVILEGES ;
+ CREATE USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
+ GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+
+ CREATE DATABASE IF NOT EXISTS `ingresse` ;
+ GRANT ALL ON `ingresse`.* TO 'root'@'%' ;
+
+ FLUSH PRIVILEGES ;
